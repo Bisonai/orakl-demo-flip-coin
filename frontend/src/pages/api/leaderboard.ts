5d71f0ebc9bdf61599ec74bcb4a1d056adf96868
Martin Kersner <martin@bisonai.com>
2024-04-06 15:19:25 +0900

    chore: move leaderboard to separate directory

--- frontend/src/pages/api/leaderboard.ts
+++ frontend/src/pages/api/leaderboard.ts
@@ -1,7 +1,6 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
-import { promises as fs } from 'fs';
-import path from 'path';
-
+import type { NextApiRequest, NextApiResponse } from "next";
+import { promises as fs } from "fs";
+import path from "path";
 
 type Player = {
   player: string;
@@ -10,15 +9,15 @@ type Player = {
   requestId: number;
   result: number;
   transaction_id?: string;
-  playAt: number
+  playAt: number;
   isWin: boolean;
-}
+};
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Player[]>
 ) {
-  const jsonDirectory = path.join(process.cwd(), 'json');
-  const fileContents = await fs.readFile(jsonDirectory + '/data.json', 'utf8');
-  res.status(200).json(JSON.parse(fileContents))
+  const jsonDirectory = path.join(process.cwd(), "leaderboard");
+  const fileContents = await fs.readFile(jsonDirectory + "/data.json", "utf8");
+  res.status(200).json(JSON.parse(fileContents));
 }
