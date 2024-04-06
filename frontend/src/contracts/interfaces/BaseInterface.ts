3ef1b1724e6c22117acaaa491ed3cbc8b3da12f7
Martin Kersner <martin@bisonai.com>
2024-04-06 15:19:39 +0900

    fix: increase gas limit

--- frontend/src/contracts/interfaces/BaseInterface.ts
+++ frontend/src/contracts/interfaces/BaseInterface.ts
@@ -20,7 +20,7 @@ export default class BaseInterface {
     this._provider = provider;
     this._contractAddress = address;
     this._abis = abi;
-    this._option = { gasLimit: 300000 };
+    this._option = { gasLimit: 400000 };
     this._contract = new ethers.Contract(address, abi, provider.getSigner());
   }
 
